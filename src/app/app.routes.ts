49025c223815e1447a31758dced64005a497d424
Hung <hung110195@gmail.com>
2025-07-20 20:22:15 +0700

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './layout/auth/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 import { ActionsMenuComponent } from './shared/components/actions-menu/actions-menu.component';
-import { MessageComponent } from './shared/components/message/message.component';
 import { ChatComponent } from './layout/chat/chat.component';
+import { MessengerComponent } from './layout/messenger/messenger.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'login', pathMatch:'full'},
     {path:'login',component:LoginComponent},
     {path:'action-menu',component: ActionsMenuComponent},
-    {path:'messenger',canActivate: [authGuard],component: MessageComponent,
+    {path:'messenger',canActivate: [authGuard],component: MessengerComponent,
       children:[
         {path:'',redirectTo:'chat',pathMatch:'full'},
         {path:'chat',component:ChatComponent},
