58426630e8cc55700092a80de31bae99272929ab
Hung <hung110195@gmail.com>
2025-07-20 23:44:53 +0700

    ok

--- src/app/shared/components/chat-box/chat-box.component.ts
+++ src/app/shared/components/chat-box/chat-box.component.ts
@@ -83,6 +83,10 @@ export class ChatBoxComponent implements OnChanges, AfterViewInit {
     setTimeout(() => {
       const message = this.chatInput.nativeElement.innerText.trim();
       this.isHiddenAction = message !== '';
+      if (event.key === 'Enter'&& this.isMobile()) {
+            event.preventDefault();
+            document.execCommand('insertHTML', false, '<br><br>');
+         }
       if (event.key === 'Enter' && !event.shiftKey && !this.isMobile()) {
         event.preventDefault();
         if (message || this.previewFile.length > 0) {
